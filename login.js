e6832938643782dca4e7d67ac31dc5342beacd13
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2022-03-31 14:03:35 +0530

    Add files via upload
    some changes

--- login.js
+++ login.js
@@ -1,72 +1,33 @@
-let t=0;
-let action=document.querySelector("#login")
-action.addEventListener("click",function(){
-   Login()
-})
 
-let Login = async ()=> {
-    try {
-        let login_data = {
-          username:document.getElementById("user").value,
-     password:document.getElementById("pass").value,
-        }
-        
-        let login_data_json = JSON.stringify(login_data);
+ let action=document.querySelector("#login")
+ action.addEventListener("click",login)
+ async function login(event){
+const email=document.getElementById("email").value;
+const password=document.getElementById("pass").value;
 
-        let res = await fetch("https://masai-api-mocker.herokuapp.com/auth/login",{
-
-            method:'POST',
-
-            body: login_data_json,
-
-            headers:{
-                "Content-Type": "application/json",
-            },
-        });
-          
-         let data = await res.json();
-         
-         console.log(data)
-         if(data.error==false){
-              t=1;
-         }
-         else{
-             alert("Invalid credentials")
-         }
-
-         getUser(login_data.username, data.token);
-       
-    } catch (error) {
-        console.log("error:", error);
-    };
-}
+const result = await fetch(`http://localhost:5003/login`,{
+    method:'post',
+    headers:{
+       'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({
+        email,
+        password
+    })  
+}).then((res)=>res.json())
+console.log(result)
+     if (result.message) {
    
-    let getUser = async (username, token) => {
-        let data_arr=[]
-        try {
-            let res = await fetch(`https://masai-api-mocker.herokuapp.com/user/${username}`,{
+    alert(result.message)
+} else {
+    // console.log(result.token)
+    localStorage.setItem('token', result.token)
+    // console.log(token)
+    window.location.href="index.html"
+}
+}
 
-              headers:{
 
-                "Content-Type": "application/json",
-                Authorization:`Bearer ${token}`,
-                },
 
-            }); 
 
-             let data = await res.json();
-             console.log("data:", data); 
-             data_arr.push(data)
-             localStorage.setItem("usertoken",JSON.stringify(data_arr))
 
-             if(t==1){
-                window.location.href="index.html"
-            }
-
-        } catch (error) {
-            console.log("error:", error);
-            
-        }
-    };
-
-    
--- signup.js
+++ signup.js
@@ -1,46 +1,34 @@
+let action=document.querySelector("#signup")
+            action.addEventListener("click",getting)
+            async function getting(event){
+                     const email=document.getElementById("email").value;
+                     const password=document.getElementById("pass").value;
+                     const username=document.getElementById("user").value;
+                     const mobile=document.getElementById("phone").value;
+           
+                   const result = await fetch(`http://localhost:5003/register`,{
+                       method:'post',
+                       headers : {
+                                   "Content-Type": "application/json",
+                          },
+                          body:
+                           JSON.stringify({
+                              email,
+                              password,
+                              username,
+                              mobile
+                           })
+                          
+                     }).then((res) => res.json())
+                     console.log(result)
+                     if (result.message) {
+				
+					alert(result.message)
+				} else {
+					
+                    window.location.href="login.html"
+				}
+            }
 
- let action=document.querySelector("#signup")
- action.addEventListener("click",function(){
-   Reg()
- })
-let Reg = async () => {
 
-    try {
-        
-      let register_data = {
 
-          name:"null",
-         email:document.getElementById("email").value,
-         password:document.getElementById("pass").value,
-         username:document.getElementById("user").value,
-         mobile:document.getElementById("phone").value,
-         description:"null",
-
-     }
-
-      register_data = JSON.stringify(register_data);
-
-        let res = await fetch("https://masai-api-mocker.herokuapp.com/auth/register", {
-
-            method:'POST',
-            body: register_data,
-
-            headers : {
-                "Content-Type": "application/json",
-            },
-        });
-        let data = await res.json();
-        console.log(data)
-         if(data.error==false){
-           window.location.href="login.html"
-         }
-        else if(data.error==true){
-          alert("Details already present")
-          console.log(data)
-        }
-        
-    } catch (error) {
-        console.log("error:", error);
-        
-    }
-}
